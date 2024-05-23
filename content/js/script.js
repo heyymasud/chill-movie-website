0ec08f8f77430cbb20fb900a303d84419cc2eb2b
heyymasud <heyymasud@gmail.com>
2024-05-24 02:21:00 +0700

    fix: script register & login

--- content/js/script.js
+++ content/js/script.js
@@ -3,65 +3,56 @@ document.addEventListener('DOMContentLoaded', function () {
     const loginForm = document.getElementById('login-form');
 
     // Register function
-    function register(event) {
-
-        try {
+    if (registerForm) {
+        registerForm.addEventListener('submit', function (event) {
             event.preventDefault();
-            const username = document.getElementById('username').value;
-            const password = document.getElementById('password').value;
-            const confirmPassword = document.getElementById('confirm-password').value;
+            try {
+                const username = document.getElementById('username').value;
+                const password = document.getElementById('password').value;
+                const confirmPassword = document.getElementById('confirm-password').value;
+                if (password !== confirmPassword) {
+                    throw new Error('Password tidak sesuai');
+                }
 
-            if (password !== confirmPassword) {
-                throw new Error('Password tidak sesuai');
+                let users = JSON.parse(localStorage.getItem('users') || '[]');
+                if (users.find(user => user.username === username)) {
+                    throw new Error('Username sudah pernah digunakan');
+                } else {
+                    users.push({ username, password });
+                    localStorage.setItem('users', JSON.stringify(users));
+                    alert('Registrasi berhasil');
+                    window.location.href = 'index.html';
+                }
+            } catch (error) {
+                alert(error.message);
             }
+        });
+    }
 
-            // Validasi username
-            let users = JSON.parse(localStorage.getItem('users') || '[]');
-
-            if (users.find(user => user.username === username)) {
-                throw new Error('Username sudah pernah digunakan');
-            } else {
-                users.push({ username, password });
-                localStorage.setItem('users', JSON.stringify(users));
-                alert('Registrasi berhasil');
-                window.location.href = 'index.html';
-            }
-
-        } catch (error) {
-            alert(error.message);
-        }
-    };
-    function login(event) {
-
-        // Login function
-
-        try {
+    // Login function
+    if (loginForm) {
+        loginForm.addEventListener('submit', function (event) {
             event.preventDefault();
-            const username = document.getElementById('login-username').value;
-            const password = document.getElementById('login-password').value;
+            try {
+                const username = document.getElementById('login-username').value;
+                const password = document.getElementById('login-password').value;
+                if (password == "" || username == "") {
+                    throw new Error('Username atau password tidak boleh kosong');
+                }
 
-            if (password == "" || username == "") {
-                throw new Error('Username atau password tidak boleh kosong');
+                let users = JSON.parse(localStorage.getItem('users') || '[]');
+                if (users.find(user => user.username === username && user.password === password)) {
+                    localStorage.setItem('loggedIn', 'true');
+                    alert('Login berhasil');
+                    window.location.href = 'index.html';
+                } else {
+                    throw new Error('Username atau password salah');
+                }
+            } catch (error) {
+                alert(error.message);
             }
-
-            // ambil data users
-            let users = JSON.parse(localStorage.getItem('users') || '[]');
-
-            if (users.find(user => user.username === username && user.password === password)) {
-                localStorage.setItem('loggedIn', 'true');
-                alert('Login berhasil');
-                window.location.href = 'index.html';
-            } else {
-                throw new Error('Username atau password salah');
-            }
-
-        } catch (error) {
-            alert(error.message);
-        }
-    };
-    registerForm.addEventListener('submit', register);
-    loginForm.addEventListener('submit', login);
+        });
+    }
 });
-
 
 
